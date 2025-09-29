8cb9724ebc2d5ce7264011cffcec4cfe64488b02
Zynith0 <nolan.lessard.music@gmail.com>
2025-09-28 23:18:11 -0400

    FEAT: added broadcasting

--- pkg/tcp/tcp.go
+++ pkg/tcp/tcp.go
@@ -3,7 +3,20 @@ package tcp
 import (
 	"fmt"
 	"net"
+	"sync"
 )
+
+type Client struct {
+	ID string
+	Conn net.Conn
+	Username string
+}
+
+var (
+	clients = make(map[string]*Client)
+	mutex sync.Mutex
+)
+
 
 func Server(port string) (net.Listener, error) {
 	listener, err := net.Listen("tcp", port)
@@ -13,4 +26,34 @@ func Server(port string) (net.Listener, error) {
 
 func Echo(conn net.Conn, message []byte) {
 	conn.Write(message)
+}
+
+func HandleConnection(conn net.Conn) {
+	c := &Client{
+		ID: conn.RemoteAddr().String(),
+		Conn: conn,
+	}
+
+	mutex.Lock()
+	clients[c.ID] = c
+	mutex.Unlock()
+
+	defer func() {
+		mutex.Lock()
+		delete(clients, c.ID)
+		mutex.Unlock()
+		conn.Close()
+	}()
+}
+
+func Broadcast(message []byte) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for _, client := range clients {
+		_, err := client.Conn.Write(message)
+		if err != nil {
+			fmt.Println("skill issue", err)
+		}
+	}
 }
